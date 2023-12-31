f64dd7f85803cf3a995bb72865e632f867d7ef52
ff1574 <125647561+ff1574@users.noreply.github.com>
2023-12-31 21:50:39 +0100

    Changed position of links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,38 +1,8 @@
 import React from "react";
-import GitHubIcon from "../Assets/Images/Icons/github.png";
-import LinkedInIcon from "../Assets/Images/Icons/linkedin.webp";
-import EmailIcon from "../Assets/Images/Icons/gmail.png";
-import MeritPagesIcon from "../Assets/Images/Icons/merit.png";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="link-container">
-        <div>
-          <a href="https://github.com/ff1574">
-            <img src={GitHubIcon} alt="GitHub" />
-            GitHub
-          </a>
-        </div>
-        <div>
-          <a href="https://linkedin.com/in/franko-fišter-b55621274">
-            <img src={LinkedInIcon} alt="LinkedIn" />
-            LinkedIn
-          </a>
-        </div>
-        <div>
-          <a href="https://meritpages.com/ff1574 ">
-            <img src={MeritPagesIcon} alt="Merit Pages" />
-            Merit Page
-          </a>
-        </div>
-        <div>
-          <a href="mailto:franko.fister@gmail.com">
-            <img src={EmailIcon} alt="Email" />
-            Gmail
-          </a>
-        </div>
-      </div>
       <p>© {new Date().getFullYear()} Franko Fišter</p>
     </footer>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,6 +6,10 @@ import {
 } from "framer-motion/dist/framer-motion";
 import { useEffect } from "react";
 import portrait from "../Assets/Images/portrait.png";
+import GitHubIcon from "../Assets/Images/Icons/github.png";
+import LinkedInIcon from "../Assets/Images/Icons/linkedin.webp";
+import EmailIcon from "../Assets/Images/Icons/gmail.png";
+import MeritPagesIcon from "../Assets/Images/Icons/merit.png";
 
 export default function Header() {
   const titleIndex = useMotionValue(0);
@@ -72,6 +76,32 @@ export default function Header() {
         />
         <h1>Franko</h1>
         <motion.h2 className="title-animation">{displayTitle}</motion.h2>
+      </div>
+      <div className="link-container">
+        <div>
+          <a href="https://github.com/ff1574">
+            <img src={GitHubIcon} alt="GitHub" />
+            GitHub
+          </a>
+        </div>
+        <div>
+          <a href="https://linkedin.com/in/franko-fišter-b55621274">
+            <img src={LinkedInIcon} alt="LinkedIn" />
+            LinkedIn
+          </a>
+        </div>
+        <div>
+          <a href="https://meritpages.com/ff1574 ">
+            <img src={MeritPagesIcon} alt="Merit Pages" />
+            Merit Page
+          </a>
+        </div>
+        <div>
+          <a href="mailto:franko.fister@gmail.com">
+            <img src={EmailIcon} alt="Email" />
+            Gmail
+          </a>
+        </div>
       </div>
     </header>
   );

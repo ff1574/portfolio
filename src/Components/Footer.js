6bb541edb26fbb087b0d0a4a9c4531c4216ac8a2
ff1574 <125647561+ff1574@users.noreply.github.com>
2023-12-31 15:38:51 +0100

    Finished animations, finished site

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,20 +1,39 @@
 import React from "react";
+import GitHubIcon from "../Assets/Images/Icons/github.png";
+import LinkedInIcon from "../Assets/Images/Icons/linkedin.webp";
+import EmailIcon from "../Assets/Images/Icons/gmail.png";
+import MeritPagesIcon from "../Assets/Images/Icons/merit.png";
 
 const Footer = () => {
   return (
     <footer>
-      <p>© {new Date().getFullYear()} Your Name</p>
-      <ul>
-        <li>
-          <a href="https://github.com/ff1574">GitHub</a>
-        </li>
-        <li>
-          <a href="https://linkedin.com/in/franko-fišter-b55621274">LinkedIn</a>
-        </li>
-        <li>
-          <a href="mailto:franko.fister@gmail.com">Email</a>
-        </li>
-      </ul>
+      <div className="link-container">
+        <div>
+          <a href="https://github.com/ff1574">
+            <img src={GitHubIcon} alt="GitHub" />
+            GitHub
+          </a>
+        </div>
+        <div>
+          <a href="https://linkedin.com/in/franko-fišter-b55621274">
+            <img src={LinkedInIcon} alt="LinkedIn" />
+            LinkedIn
+          </a>
+        </div>
+        <div>
+          <a href="https://meritpages.com/ff1574">
+            <img src={MeritPagesIcon} alt="Merit Pages" />
+            Merit Page
+          </a>
+        </div>
+        <div>
+          <a href="mailto:franko.fister@gmail.com">
+            <img src={EmailIcon} alt="Email" />
+            Gmail
+          </a>
+        </div>
+      </div>
+      <p>© {new Date().getFullYear()} Franko Fišter</p>
     </footer>
   );
 };
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion/dist/framer-motion";
 import pokedex from "../Assets/Images/pokedex.jpg";
 import pizzeria from "../Assets/Images/pizzeria.png";
 import amongus from "../Assets/Images/among-us.jpg";
@@ -7,46 +8,48 @@ import openai from "../Assets/Images/openai.png";
 import dadbot from "../Assets/Images/dad-bot.jpg";
 
 const Projects = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+
   const projects = [
     {
       name: "Pokedex App",
       description:
-        "Created a mobile app using React Native that connects to PokeAPI REST which displays Pokemon and information about them.",
+        "This was definitely a really fun project that I did in my free time. I wanted to learn React Native so I've created a React Native app that connects to PokeAPI and displays all the original 151 Pokemon and their statistics. It was created in React Native / Expo.",
       link: "https://github.com/ff1574/pokedex",
       image: pokedex,
     },
     {
       name: "Pizzeria Website",
       description:
-        "Created a responsive website for Il Mondo Pizzeria using HTML, CSS, and JS.",
+        "This website was created for Pizzeria Il Mondo, a local pizzeria in Zagreb, Croatia. The plan is to soon recreate it using better technologies and make it work for online orders, as currently it is just a non interactible menu.",
       link: "http://www.ilmondopizzeria.hr/",
       image: pizzeria,
     },
     {
       name: "Among Us Game",
       description:
-        "Created an “Among Us”-like game with a college classmate using JavaFX from scratch.",
+        "This was a final project for a semester, the task was to create an 'Among Us' game using JavaFX. The project couldn't be finished in time so you can only do a pacifist run :)",
       link: "https://github.com/ff1574/among-us",
       image: amongus,
     },
     {
       name: "Tunisia Website",
       description:
-        "Created a responsive website for an individual college project using PHP, HTML, CSS, and JS.",
+        "This is one of the college projects that I am proud of as it was my first website where I started learning how to create cool effects with only base HTML, CSS, JS, and PHP.",
       link: "https://github.com/ff1574/tunisia-website",
       image: tunisia,
     },
     {
       name: "OpenAI API App",
       description:
-        "Created a JavaFX app that connects to the OpenAI API, allows manipulating the AI response via sliders for temperature and max tokens.",
+        "This was a quick and fun project during the chaos of the final exams. I've created a JavaFX app that connects to OpenAI API and allows you to manipulate the response through sliders. It was created in Java / JavaFX.",
       link: "https://github.com/ff1574/open-AI",
       image: openai,
     },
     {
       name: "Discord Bot",
       description:
-        "As a personal project I have created a discord bot using JS, hosted off my computer via Node.js.",
+        "One of my first projects ever, I've created a Discord bot using Node.js that responds to messages with dad jokes. It was made as a fun running joke between friends and still lives on a private Discord server. It was created in Javascript and hosted with Node.js",
       link: "https://github.com/ff1574/dad-bot",
       image: dadbot,
     },
@@ -58,18 +61,56 @@ const Projects = () => {
       <h2>My Projects</h2>
       <div className="projects-grid">
         {projects.map((project, index) => (
-          <div key={index} className="project-item">
+          <div
+            key={index}
+            className="project-item"
+            onClick={() => setSelectedProject(project)}
+          >
             <div className="project-image">
               <img src={project.image} alt={project.name} />
               <div className="project-name">{project.name}</div>
             </div>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">
-              Link
-            </a>
           </div>
         ))}
       </div>
+
+      {selectedProject && (
+        <div className="project-modal" onClick={() => setSelectedProject(null)}>
+          <motion.div
+            className="project-modal-content"
+            onClick={(e) => e.stopPropagation()}
+            initial={{ opacity: 0, y: "-100vh" }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transitionEnd: {
+                x: [0, -10, 10, -10, 10, 0],
+                transition: { duration: 0.5 },
+              },
+            }}
+            exit={{ opacity: 0, y: "100vh" }}
+          >
+            <span className="close" onClick={() => setSelectedProject(null)}>
+              &times;
+            </span>
+            <img
+              className="project-modal-image"
+              src={selectedProject.image}
+              alt={selectedProject.name}
+            />
+            <h2 className="project-modal-name">{selectedProject.name}</h2>
+            <p className="project-modal-description">
+              {selectedProject.description}
+            </p>
+            <button
+              className="animated-button"
+              onClick={() => window.open(selectedProject.link, "_blank")}
+            >
+              Link
+            </button>
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 };
